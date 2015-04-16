98be2cefa7ac2db0cf7d344a6778bb9f8f3788b6
sangames <s.saminathan@gmail.com>
2015-04-16 22:56:08 +0530

    Update PluginResourceToString for Tabs

--- snippets/code/PluginResourceToString.java
+++ snippets/code/PluginResourceToString.java
@@ -1,19 +1,19 @@
 class PluginResourceToString {
 	private String convertResourceToString(final String filePath) {
-        URL url = FrameworkUtil.getBundle(getClass()).getEntry(filePath);
-
-        if (url != null) {
-            try {
-                InputStream inputStream = url.openStream();                
-                FileConverToString util = new FileToString();
-                String readMeStr = util.convertToString(inputStream);
-                return readMeStr;
-            } catch (IOException e) {
-                e.printStackTrace();
+	        URL url = FrameworkUtil.getBundle(getClass()).getEntry(filePath);
+	
+	        if (url != null) {
+	            try {
+	                InputStream inputStream = url.openStream();                
+	                FileConverToString util = new FileToString();
+	                String readMeStr = util.convertToString(inputStream);
+	                return readMeStr;
+	            } catch (IOException e) {
+	                e.printStackTrace();
+					return null;
+	            }
+			} else {
 				return null;
-            }
-		} else {
-			return null;
+			}
 		}
-	}
 }
