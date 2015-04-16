2ec784e48ab0a46c85319d48b1ea4201c9894992
sangames <s.saminathan@gmail.com>
2015-04-16 23:04:07 +0530

    Added documentation

--- snippets/code/PluginResourceToString.java
+++ snippets/code/PluginResourceToString.java
@@ -1,3 +1,14 @@
+/**
+ * Method which reads from a text resource in to string.
+ * Tried the below already but FileLocator.resolve is not working correctly, when the product is exported. Hence the 
+ * code snippet has the working code.
+ * URI fileURI = FileLocator.resolve(readMeURL).toURI();
+ * Path path = Paths.get(fileURI);
+ * String filePath = path.toString();
+ */
+ 
+
+
 class PluginResourceToString {
 	private String convertResourceToString(final String filePath) {
 	        URL url = FrameworkUtil.getBundle(getClass()).getEntry(filePath);
